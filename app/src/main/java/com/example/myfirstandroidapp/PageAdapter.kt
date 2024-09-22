14b69b011e2edee29221febb6c81ba4ace409841
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-09-23 04:30:40 +0900

    update: PageAdapterをFragmentStateAdapterで拡張

--- app/src/main/java/com/example/myfirstandroidapp/PageAdapter.kt
+++ app/src/main/java/com/example/myfirstandroidapp/PageAdapter.kt
@@ -2,6 +2,7 @@ package com.example.myfirstandroidapp
 
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class PageAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) {
+class PageAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle): FragmentStateAdapter {
 }
