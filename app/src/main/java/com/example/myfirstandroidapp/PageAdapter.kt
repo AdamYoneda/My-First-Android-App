22b1c9d901352a58ac440fb327f066f034d68a6d
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-09-23 04:51:40 +0900

    update: FragmentStateAdapterにfragmentManagerとlifecycleを渡す

--- app/src/main/java/com/example/myfirstandroidapp/PageAdapter.kt
+++ app/src/main/java/com/example/myfirstandroidapp/PageAdapter.kt
@@ -4,5 +4,6 @@ import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class PageAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle): FragmentStateAdapter {
+class PageAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle):
+    FragmentStateAdapter(fragmentManager, lifecycle) {
 }
