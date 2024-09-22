99218005e2b14ff2478f3fb1eaa78e519462be4b
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-09-23 04:59:43 +0900

    update: Implement memberを行う

--- app/src/main/java/com/example/myfirstandroidapp/PageAdapter.kt
+++ app/src/main/java/com/example/myfirstandroidapp/PageAdapter.kt
@@ -1,9 +1,17 @@
 package com.example.myfirstandroidapp
 
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class PageAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle):
     FragmentStateAdapter(fragmentManager, lifecycle) {
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        TODO("Not yet implemented")
+    }
 }
